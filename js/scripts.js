22a505c07425eaf3a1055c29e1f65cd4159cd7af
Andrew Merrell and Kevin Johnston <kevin@>
2016-08-11 15:49:34 -0700

    Add error class for name input and additional branching for survey results

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,36 @@ $(document).ready(function() {
     var lunchValue = parseInt($("#lunch").val());
     var sleepValue = parseInt($("#sleep").val());
     var result = moodValue + lunchValue + sleepValue;
+    var nameInput = $("input#name").val();
 
-    if (result >=3 && result <6 ) {
+
+    $(".name").text(nameInput);
+
+    if (nameInput === "") {
+      $(".error").show();
+      $('#sloth').hide();
+      $('#bizCat').hide();
+      $('#manBearPig').hide();
+    } else if (result >=3 && result <6 ) {
       $('#sloth').show();
       $('#bizCat').hide();
       $('#manBearPig').hide();
+      $(".error").hide();
     } else if (result >=6 && result <8){
       $('#bizCat').show();
       $('#sloth').hide();
       $('#manBearPig').hide();
+      $(".error").hide();
     } else {
       $('#manBearPig').show();
       $('#sloth').hide();
       $('#bizCat').hide();
+      $(".error").hide();
     };
+
+
+
+
 
   });
 });
