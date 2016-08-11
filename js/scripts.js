a1b49938bb731d6ddb83f25dcfa987ba7ed603aa
Andrew Merrell and Kevin Johnston <kevin@>
2016-08-11 11:50:39 -0700

    Add jQuery function with branching and proper formatting in css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-  ("form").submit(function(event) {
-    event.preventDefault();
+  $("#blanks form").submit(function(event){
+      event.preventDefault();
+
+    var moodValue = parseInt($("input:radio[name=mood]:checked").val());
+
+    if (moodValue === 1) {
+      $('#sloth').show();
+      $('#bizCat').hide();
+      $('#manBearPig').hide();
+    } else if (moodValue === 2){
+      $('#bizCat').show();
+      $('#sloth').hide();
+      $('#manBearPig').hide();
+    } else {
+      $('#manBearPig').show();
+      $('#sloth').hide();
+      $('#bizCat').hide();
+    };
+
+
   });
 });
